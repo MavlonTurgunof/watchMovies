573fcbca41e4ab0a5de4135c877343e0f4c9f593
“wiut00012220” <wiut12220@gmail.com>
2023-12-17 22:14:53 +0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+// import StarRating from "./StarRating";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+    {/* <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} */}
+    {/* /> */}
+  </React.StrictMode>
+);
