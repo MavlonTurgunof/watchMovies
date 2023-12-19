1ce45a91f704c318cd41b73bf1362725d1f23b58
“wiut00012220” <wiut12220@gmail.com>
2023-12-20 01:06:13 +0500

    cleanup functions implemented

--- src/App.js
+++ src/App.js
@@ -36,12 +36,14 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
           if (!res.ok) throw new Error("something went wrong");
           const data = await res.json();
@@ -49,7 +51,10 @@ export default function App() {
           setMovies(data.Search);
         } catch (err) {
           console.error(err.message);
-          setError(err.message);
+
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -60,6 +65,10 @@ export default function App() {
         return;
       }
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
@@ -226,6 +235,17 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     onAddWatched(newWatchedMovie);
     onCloseMovie();
   }
+
+  useEffect(
+    function () {
+      if (!title) return;
+      document.title = `Movie| ${title}`;
+      return function () {
+        document.title = "MovieInfo";
+      };
+    },
+    [title]
+  );
 
   useEffect(
     function () {
